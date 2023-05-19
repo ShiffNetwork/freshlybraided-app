d42d760440c343782091a071ae29ce757bd12418
ShiffNetwork <chirroc.breell@gmail.com>
2023-05-19 12:18:35 -0400

    first big commit

---
+++ resources/js/Pages/Appointments/Index.vue
@@ -0,0 +1,48 @@
+<template>
+    <MainLayout>
+        <main class="flex-1 max-h-full p-5 overflow-hidden">
+            <!-- Main content header -->
+            <div
+                class="flex flex-col items-start justify-between pb-6 space-y-4 border-b lg:items-center lg:space-y-0 lg:flex-row">
+                <h1 class="text-2xl font-semibold whitespace-nowrap">Appointments</h1>
+                <div class="space-y-6 md:space-x-2 md:space-y-0">
+                    <a href="#" target="_blank"
+                        class="inline-flex items-center justify-center px-4 py-1 space-x-1 bg-gray-200 rounded-md shadow hover:bg-opacity-20">
+                        <span> + Add new Appointment</span>
+                    </a>
+                </div>
+            </div>
+
+            <!-- Start Content -->
+            <h2 class="mt-6 text-xl">Future Appointments</h2>
+            <div class="grid grid-cols-1 gap-5 mt-6 sm:grid-cols-2 lg:grid-cols-4">
+                <!-- <template x-for="i in 4" :key="i"> -->
+                <div class="p-4 transition-shadow border rounded-lg shadow-sm hover:shadow-lg">
+                    <div class="flex items-start justify-between">
+                        <div class="flex flex-col space-y-2">
+                            <span class="text-lg font-bold">Chirroc Breell</span>
+                            <span class="text-md font-semibold"> 19 May 2023</span>
+                            <span>80.00 €</span>
+                            <span>Medium box Braids</span>
+                        </div>
+                        <div>
+                            <span class="text-indigo-500 text-xl gap-2">
+                                <ion-icon name="create-outline"></ion-icon>
+                            </span>
+                            <span class="text-red-500 text-xl gap-2">
+                                <ion-icon name="trash-outline"></ion-icon>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- </template> -->
+            </div>
+        </main>
+    </MainLayout>
+</template>
+
+
+<script setup>
+import MainLayout from '../../Layouts/MainLayout.vue';
+</script>
---
+++ resources/js/Pages/Hairstyles/Index.vue
@@ -0,0 +1,73 @@
+<template>
+    <MainLayout>
+        <main class="flex-1 max-h-full p-5 overflow-hidden">
+            <div
+                class="flex flex-col items-start justify-between pb-6 space-y-4 border-b lg:items-center lg:space-y-0 lg:flex-row">
+                <h1 class="text-2xl font-semibold whitespace-nowrap">Hairstyles</h1>
+                <div class="space-y-6 md:space-x-2 md:space-y-0">
+                    <a href="#" target="_blank"
+                        class="inline-flex items-center justify-center px-4 py-1 space-x-1 bg-gray-200 rounded-md shadow hover:bg-opacity-20">
+                        <span>+ Add new Hairstyles</span>
+                    </a>
+                </div>
+            </div>
+            <div class="flex flex-col mt-6">
+                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                        <div class="overflow-hidden border-b border-gray-200 rounded-md shadow-md">
+                            <table class="min-w-full overflow-x-scroll divide-y divide-gray-200">
+                                <thead class="bg-gray-50">
+                                    <tr>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+                                            Hairstyle name
+                                        </th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+                                            Price
+                                        </th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+                                            Length
+                                        </th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+                                            Actions
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody class="bg-white divide-y divide-gray-200">
+                                    <!-- <template x-for="i in 10" :key="i"> -->
+                                    <tr class="transition-all hover:bg-gray-100 hover:shadow-lg">
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">Medium Box braid</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm font-medium text-gray-900">80.00€</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm font-medium text-gray-900">3h</div>
+                                        </td>
+                                        <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
+                                            <span class="text-indigo-500 text-xl gap-2">
+                                                <ion-icon name="create-outline"></ion-icon>
+                                            </span>
+                                            <span class="text-red-500 text-xl gap-2">
+                                                <ion-icon name="trash-outline"></ion-icon>
+                                            </span>
+                                        </td>
+                                    </tr>
+                                    <!-- </template> -->
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from '../../Layouts/MainLayout.vue';
+</script>
